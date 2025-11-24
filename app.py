ce1c9febd064473a5de9f01fac1074c8379861f1
MarianoCG1 <141775512+MarianoCG1@users.noreply.github.com>
2025-11-24 11:58:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -470,7 +470,92 @@ if st.session_state.step == 1:
                 if csv_file:
                     # Extraer el archivo CSV desde el ZIP y leerlo en un DataFrame
                     with zip_ref.open(csv_file) as f:
-                        df = pd.read_csv(f)
+                        # Extraer el archivo CSV desde el ZIP y leerlo en un DataFrame
+                    with zip_ref.open(csv_file) as f:
+                        columnas_geografia = [
+                            "NOMBREDD", "NOMBREPP", "NOMBREDI",
+                            "CCDD", "CCPP", "CCDI"
+                        ]
+
+                        columnas_peso = [
+                            "FACTOR"
+                        ]
+
+                        columnas_confianza_declarada = [
+                            "P608_1", "P608_2", "P608_3", "P608_4"
+                        ]
+
+                        columnas_desempeno = [
+                            "P612_1", "P612_2", "P612_3", "P612_4", "P613"
+                        ]
+
+                        columnas_presencia_existencia = [
+                            "P642_1", "P642_2", "P642_3"
+                        ]
+
+                        columnas_presencia_calidad = [
+                            "P643_1", "P643_2", "P643_3"
+                        ]
+
+                        columnas_presencia_meses = [
+                            "P644_1", "P644_2", "P644_3"
+                        ]
+
+                        columnas_expectativa = [
+                            f"P601_{i}" for i in range(1, 17)  # por seguridad, capturamos P601_1 hasta P601_19
+                        ]
+
+                        columnas_inseguridad_directa = [
+                            "P602",  # inseguridad en barrio
+                            "P604",  # inseguridad de noche
+                            "P605"   # inseguridad de día
+                        ]	
+
+                        columnas_lugares = [
+                            f"P606_{i}" for i in range(1, 11)   # por seguridad, P606_1 a P606_39
+                        ]
+
+
+                        # 3) Columnas P615 (bandera: fue víctima 1=Sí 2=No)
+                        # (capturamos un rango amplio para seguridad)
+                        # --------------------------
+                        columnas_p615 = ['P615_1', 'P615_2', 'P615_3', 'P615_4', 'P615_5', 
+                        'P615_6', 'P615_7', 'P615_8', 'P615_9', 'P615_10', 'P615_11', 'P615_12', 
+                        'P615_13', 'P615_14', 'P615_15', 'P615_16', 'P615_17', 'P615_18', 'P615_19', 
+                        'P615_20', 'P615_21', 'P615_22', 'P615_23', 'P615_24', 'P615_25', 'P615_26', 'P615_27', 'P615_28']
+
+
+                        # --------------------------
+                        # 4) Columnas P616 (conteo: nº de veces)
+                        # (mismo rango amplio)
+                        # --------------------------
+                        columnas_p616 = ['P616_1', 'P616_2', 'P616_3', 'P616_4', 'P616_5', 
+                        'P616_6', 'P616_7', 'P616_8', 'P616_9', 'P616_10', 'P616_11', 'P616_12', 
+                        'P616_13', 'P616_14', 'P616_15', 'P616_16', 'P616_17', 'P616_18', 
+                        'P616_19', 'P616_20', 'P616_21', 'P616_22', 'P616_23', 'P616_24', 
+                        'P616_25', 'P616_26', 'P616_27', 'P616_28']
+
+
+                        columnas_requeridas = (
+                            columnas_geografia
+                            + columnas_peso
+                            + columnas_confianza_declarada
+                            + columnas_desempeno
+                            + columnas_presencia_existencia
+                            + columnas_presencia_calidad
+                            + columnas_presencia_meses
+                            + columnas_expectativa
+                            + columnas_inseguridad_directa
+                            + columnas_lugares
+                            +columnas_p615
+                            +columnas_p616)
+
+
+                        df = pd.read_csv(
+                            f,
+                            usecols=columnas_requeridas,   # SOLO las que necesitas
+                            low_memory=False
+                        )
 
                     # Botón para procesar el CSV
                     if st.button("Procesar Datos"):
